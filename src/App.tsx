cbc4b10abbd37d4de4b53cdbd5bba291cea97494
Andre Gouws <agouws@amazon.com>
2025-04-04 17:53:11 -0400

    Initial website creation

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>shane julia wedding lmao</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
